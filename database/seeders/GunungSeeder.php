a5e0c535012fef6ca6531674f3e2cf47c43ced2c
Faiz Ismail <faizismail1706@gmail.com>
2024-11-08 11:09:57 +0700

    beranda dah bisa

--- database/seeders/GunungSeeder.php
+++ database/seeders/GunungSeeder.php
@@ -20,30 +20,30 @@ class GunungSeeder extends Seeder
                 'deskripsi' => 'Gunung Merbabu adalah gunung berapi bertipe stratovolcano yang terletak di Jawa Tengah.',
                 'gambar' => 'http://localhost:8000/images/img_image.png', // Perbaiki kesalahan ketik
                 'ketinggian' => 3142,
-                'province_id' => 1, // Sesuaikan jika ada
-                'regency_id' => null, // Sesuaikan jika ada
-                'district_id' => null, // Sesuaikan jika ada
-                'village_id' => null, // Sesuaikan jika ada
+                'province_id' => 33, // Sesuaikan jika ada
+                'regency_id' => 3328, // Sesuaikan jika ada
+                'district_id' => 332815, // Sesuaikan jika ada
+                'village_id' => 3328152007, // Sesuaikan jika ada
             ],
             [
                 'nama' => 'Gunung Slamet',
                 'deskripsi' => 'Gunung Slamet adalah gunung berapi tertinggi di Jawa Tengah.',
                 'gambar' => 'http://localhost:8000/images/img_image_158x314.png',
                 'ketinggian' => 3428, // Update jika perlu
-                'province_id' => 1,
-                'regency_id' => null,
-                'district_id' => null,
-                'village_id' => null,
+                'province_id' => 33,
+                'regency_id' => 3328,
+                'district_id' => 332815,
+                'village_id' => 3328152007,
             ],
             [
                 'nama' => 'Gunung Sumbing',
                 'deskripsi' => 'Gunung Sumbing adalah gunung berapi yang terletak di Jawa Tengah.',
                 'gambar' => 'http://localhost:8000/images/img_image_156x316.png',
                 'ketinggian' => 3371, // Update jika perlu
-                'province_id' => 1,
-                'regency_id' => null,
-                'district_id' => null,
-                'village_id' => null,
+                'province_id' => 33,
+                'regency_id' => 3328,
+                'district_id' => 332815,
+                'village_id' => 3328152007,
             ],
         ]);
     }
